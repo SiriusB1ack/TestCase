560ff150c5d5815e1f5bd1069e13e85177e8e1f1
SiriusB1ack <agentoo@mail.ru>
2016-01-06 23:07:54 +0200

    first version
    first version

---
+++ WebApplication2/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table border="3" width ="600">
+            <tr>
+                <td>Less 10Mb</td>
+                <td>10Mb-50Mb</td>
+                <td>More 100Mb</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Sma" runat="server" Text="Label"></asp:Label>
+                </td>
+                <td>
+                    <asp:Label ID="Ave" runat="server" Text="Label"></asp:Label>
+                </td>
+                <td>
+                    <asp:Label ID="Bii" runat="server" Text="Label"></asp:Label>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <asp:Label ID="Curr" runat="server" Text=""></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Dirs" runat="server" Text=""></asp:Label>
+
+        <br />
+        <br />
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebApplication2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int small = 0;
+            int average = 0;
+            int big = 0;
+            string strpath = "";
+            HttpCookie reqCookie = Request.Cookies["path"];
+            if (reqCookie != null)
+            {
+                strpath = reqCookie.Value;
+            }
+            else
+            {
+                strpath = AppDomain.CurrentDomain.BaseDirectory;
+            }
+            //strpath = AppDomain.CurrentDomain.BaseDirectory;
+            string reqDir = Request.QueryString["dir"];
+            if (reqDir != null)
+            {
+                string finalStrpath = strpath + reqDir + @"\";
+                if (Directory.Exists(finalStrpath))
+                {
+                    if (reqDir == "..")
+                    {
+                        try
+                        {
+                            strpath = Directory.GetParent(strpath).FullName;
+                        }
+                        catch
+                        {
+                            
+                        }
+                    }
+                    else
+                    {
+                        strpath = finalStrpath;
+                    }
+                }
+            }
+
+            string[] allFoundFiles = Directory.GetFiles(strpath, "*.*", SearchOption.AllDirectories);
+            foreach (string file in allFoundFiles)
+            {
+                FileInfo tmpfl = new FileInfo(file);
+                if (tmpfl.Length <= 10 * 1024 * 1024)
+                { small++; }
+                else if (tmpfl.Length >= 100 * 1024 * 1024)
+                { big++; }
+                else if (tmpfl.Length > 10 * 1024 * 1024 && tmpfl.Length <= 50 * 1024 * 1024)
+                { average++; }
+
+            }
+            Sma.Text = small.ToString();
+            Ave.Text = average.ToString();
+            Bii.Text = big.ToString();
+            Curr.Text = "<b>Current path: </b>" + strpath + "<br />";
+            DirectoryInfo di = new DirectoryInfo(strpath);
+            Dirs.Text += ("<a href = 'Default.aspx?dir=..'> ..</a><br/>");
+           
+                foreach (var fi in di.GetDirectories())
+                {
+                    Dirs.Text += ("<a href = 'Default.aspx?dir=" + fi.Name + "'> " + fi.Name + "</a><br/>");
+                }
+                Dirs.Text += "<br/>";
+            foreach (var fi in di.GetFiles())
+            {
+                Dirs.Text += ("<a href = '" + strpath + fi.Name + "'> "  + fi.Name + "</a><br/>");
+            }
+            HttpCookie cookie = new HttpCookie("path", strpath);
+            Response.Cookies.Add(cookie);
+        }
+
+        
+    }
+}
